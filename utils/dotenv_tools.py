b0f5cf2f7ad563aa445addb7b81a946c4b7b306e
Your Name <you@example.com>
2026-05-09 20:11:21 +0300

    fea: dotenv_tools was updated

--- utils/dotenv_tools.py
+++ utils/dotenv_tools.py
@@ -6,13 +6,17 @@ from dotenv import load_dotenv, set_key
 from config import DOTENV_FILE_PATH
 
 load_dotenv()
-def init() -> None:
-    if not os.path.exists(DOTENV_FILE_PATH):
-        keys = (("API_ID", 1), ("API_HASH", 1), ("PROXY_URL", 0))
-        for key, mandatory in keys:
-            if not mandatory:
-                if not input(f"Желаете ли установить {key}? (Y/n): ").lower() == "y":
-                    continue
-            
-            value = getpass(f"Введите {key}: ")
-            set_key(DOTENV_FILE_PATH, key, value)
+def init() -> None:    
+    keys = (("API_ID", 1, 1), ("API_HASH", 1, 1), ("PROXY_URL", 0, 0), ("CHATS_TO_HANDLE", 1, 0))
+
+    for key, mandatory, hide in keys:
+        if not mandatory or (mandatory and os.getenv(key) is not None):
+            continue
+        if not mandatory:
+            if not input(f"Желаете ли установить {key}? (Y/n): ").lower() == "y":
+                continue
+        
+        value = getpass(f"Введите {key}: ") if hide else input(f"Введите {key}: ")
+        set_key(DOTENV_FILE_PATH, key, value)
+
+    load_dotenv()
